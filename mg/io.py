1ef38c4fb142dc1aa171b934842b526be9f1992f
Matthew Farrugia-Roberts <matt@far.in.net>
2020-12-02 17:43:49 +1100

    refactor io

--- mg/io.py
+++ mg/io.py
@@ -1,26 +1,27 @@
 import re
 import sys
 
-from prompt_toolkit import prompt as _prompt, print_formatted_text
+from prompt_toolkit import prompt as ptk_prompt
+from prompt_toolkit import print_formatted_text as ptk_print
 from prompt_toolkit.formatted_text import HTML, to_formatted_text
 
-_print = print
-_input = input
+std_print = print
+std_input = input
 htmltag = re.compile(r"</?[^>]+>")
 
 if sys.stdout.isatty():
     def print(*args, **kwargs):
-        print_formatted_text(*[HTML(str(s)) for s in args], **kwargs)
+        ptk_print(*[HTML(str(s)) for s in args], **kwargs)
 else:
     def print(*args, **kwargs):
-        _print(*[htmltag.sub("", s) for s in args], **kwargs)
+        std_print(*[htmltag.sub("", s) for s in args], **kwargs)
 
 if sys.stdin.isatty():
     def input(prompt, r=None):
-        return _prompt(prompt + " ", rprompt=r)
+        return ptk_prompt(prompt + " ", rprompt=r)
 else:
     def input(prompt, r=None):
         if r is not None:
             print(r)
-        return _input(prompt + " ")
+        return std_input(prompt + " ")
 
