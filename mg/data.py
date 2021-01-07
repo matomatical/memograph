979abfc388eafa446be145471e38a6f297f7a989
Matthew Farrugia-Roberts <matt@far.in.net>
2021-01-07 17:36:25 +1100

    extra data format functionality

--- mg/data.py
+++ mg/data.py
@@ -6,4 +6,12 @@ def parse(links_text, sep="--", skip_header=True):
     for line in links_text.splitlines()[skip_header:]:
         if sep not in line:
             continue
-        yield tuple(map(str.strip, line.split("#")[0].split(sep)))
+        # remove comments, whitespace
+        line = line.split("#", maxsplit=1)[0].strip()
+        fields = tuple(map(str.strip, line.split(sep)))
+        if fields[0] == "":
+            fields = prefix + fields[1:]
+        if fields[-1] == "":
+            prefix = fields[:-1]
+        else:
+            yield fields
